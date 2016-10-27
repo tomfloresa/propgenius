20a2eb48c8fc09e89e47afd12c333832d72f1a38
Tomás Flores Alvear <tomas.flores.a@gmail.com>
2016-10-27 00:03:51 -0300

    visual changes

---
+++ app/assets/javascripts/transversals.js
@@ -0,0 +1,6 @@
+$(document).on('turbolinks:load', function() {
+  if ($('#common_expense_property_period_2i') && $('#common_expense_property_period_1i')) {
+    $('#common_expense_property_period_2i').wrap("<div class='col-lg-6 col-md-6 col-sm-6 col-xs-12'></div>");
+    $('#common_expense_property_period_1i').wrap("<div class='col-lg-6 col-md-6 col-sm-6 col-xs-12'></div>");
+  }
+});
