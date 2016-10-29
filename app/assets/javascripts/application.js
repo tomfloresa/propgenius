8b8c7685555a7f099e7d11491aa1270f5591251f
Tomás Flores Alvear <tomas.flores.a@gmail.com>
2016-10-29 00:56:38 -0300

    several changes in views and added chart.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,5 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap/dist/js/bootstrap.min.js
+//= require chart.js/dist/Chart
 //= require_tree .
--- app/assets/javascripts/transversals.js
+++ app/assets/javascripts/transversals.js
@@ -1,6 +1,63 @@
 $(document).on('turbolinks:load', function() {
-  if ($('#common_expense_property_period_2i') && $('#common_expense_property_period_1i')) {
-    $('#common_expense_property_period_2i').wrap("<div class='col-lg-6 col-md-6 col-sm-6 col-xs-12'></div>");
-    $('#common_expense_property_period_1i').wrap("<div class='col-lg-6 col-md-6 col-sm-6 col-xs-12'></div>");
-  }
+    if ($('#common_expense_property_period_2i') && $('#common_expense_property_period_1i')) {
+        $('#common_expense_property_period_2i').wrap("<div class='col-lg-6 col-md-6 col-sm-6 col-xs-12'></div>");
+        $('#common_expense_property_period_1i').wrap("<div class='col-lg-6 col-md-6 col-sm-6 col-xs-12'></div>");
+    }
+});
+$(document).on('turbolinks:load', function() {
+    var ctx = document.getElementById("property-types-chart");
+    var myChart = new Chart(ctx, {
+        type: 'pie',
+        data: {
+            labels: [
+                "Red",
+                "Blue",
+                "Yellow",
+                "Green",
+                "Purple",
+                "Orange"
+            ],
+            datasets: [
+                {
+                    label: '# of Votes',
+                    data: [
+                        12,
+                        19,
+                        3,
+                        5,
+                        2,
+                        3
+                    ],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }
+            ]
+        },
+        options: {
+            scales: {
+                yAxes: [
+                    {
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }
+                ]
+            }
+        }
+    });
 });
