ebcffa970f822f050fc87e7912c1a93a0cba233d
Tomás Flores Alvear <tomas.flores.a@gmail.com>
2017-01-11 20:47:30 -0300

    fixed height for squares

--- app/controllers/renters_controller.rb
+++ app/controllers/renters_controller.rb
@@ -1,6 +1,16 @@
 class RentersController < ApplicationController
   def dashboard
-    #code
+    ## Get all subunits for current renter
+    @subunits = current_renter.subunits
+    
+    ## Get all unpayed subunit_rents
+    subunits = current_renter.subunits
+
+    subunits.each do |s|
+      @subunits_debts = SubunitRent.where(subunit_id: s.id).where(payed: false)
+    end
+
+    puts @subunits_debts.count
   end
 
   def index
