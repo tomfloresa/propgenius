a0d9835c1f0d0f1df5dc218bdd9acb91dac6df5f
Tomás Flores Alvear <tomas.flores.a@gmail.com>
2017-01-12 19:20:32 -0300

    fixed charge for overdue rent

--- app/models/subunit_rent.rb
+++ app/models/subunit_rent.rb
@@ -3,11 +3,21 @@ class SubunitRent < ActiveRecord::Base
   belongs_to :subunit
 
   def get_overdue_charge
+    ## Get the month of charge
     period_month = self.period.month
+
+    ## Get the year of charge
     period_year = self.period.year
+
+    ## Get the payment deadline
     deadline_date = Date.parse("10-#{period_month}-#{period_year}")
-    if Date.today > deadline_date && !self.payed?
+
+    ## Calculate charge
+    if Date.today > deadline_date && Date.today <= deadline_date.end_of_month && !self.payed?
       @number_of_days_overdue = (Date.today - deadline_date).to_i
+      @overdue_charge = (self.amount * 0.01) * @number_of_days_overdue
+    elsif Date.today > deadline_date.end_of_month
+      @number_of_days_overdue = (deadline_date.end_of_month - deadline_date).to_i
       @overdue_charge = (self.amount * 0.01) * @number_of_days_overdue
     else
       0
