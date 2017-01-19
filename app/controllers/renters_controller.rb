f72759f6e29c54489e819857045b155933a6c8cb
tomfloresa <tomas.flores.a@gmail.com>
2017-01-18 23:24:04 -0300

    fixed views

--- app/controllers/renters_controller.rb
+++ app/controllers/renters_controller.rb
@@ -2,15 +2,13 @@ class RentersController < ApplicationController
   def dashboard
     ## Get all subunits for current renter
     @subunits = current_renter.subunits
-    
+
     ## Get all unpayed subunit_rents
     subunits = current_renter.subunits
 
     subunits.each do |s|
       @subunits_debts = SubunitRent.where(subunit_id: s.id).where(payed: false)
     end
-
-    puts @subunits_debts.count
   end
 
   def index
