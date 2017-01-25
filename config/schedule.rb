18cd72035d56f78ed69a30044ec38ddc8b26b582
Tomás Andrés Flores Alvear <tomfloresa@Tomass-MBP.home>
2017-01-25 18:24:22 -0300

    added cron jobs

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,6 @@
 every 5.minutes do
-  Subunit.notify_when_thirty_to_end_lease
-  Subunit.notify_when_fifteen_to_end_lease
-  Subunit.notify_when_five_to_end_lease
-  Subunit.notify_when_one_to_end_lease
+  runner "Subunit.notify_when_thirty_to_end_lease"
+  runner "Subunit.notify_when_fifteen_to_end_lease"
+  runner "Subunit.notify_when_five_to_end_lease"
+  runner "Subunit.notify_when_one_to_end_lease"
 end
