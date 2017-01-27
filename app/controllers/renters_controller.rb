c4f0bdfa2c7c20b3a7150b34be9c5d1079c970cc
Tomás Andrés Flores Alvear <tomfloresa@Tomass-MBP.home>
2017-01-27 00:22:17 -0300

    building renter layout

--- app/controllers/renters_controller.rb
+++ app/controllers/renters_controller.rb
@@ -1,7 +1,11 @@
 class RentersController < ApplicationController
+  before_action :authenticate_renter!
   def dashboard
     ## Get all subunits for current renter
     @subunits = current_renter.subunits
+
+    ## Get all properties holding those subunits
+    @properties = current_renter.subunits.pluck(:property_id).uniq!
 
     ## Get all unpayed subunit_rents
     subunits = current_renter.subunits
