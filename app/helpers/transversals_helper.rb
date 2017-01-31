05891001799300c0b07962903ec153b5fc850b61
Tomás Andrés Flores Alvear <tomfloresa@Tomass-MBP.home>
2017-01-31 15:20:45 -0300

    fixing shared views

--- app/helpers/transversals_helper.rb
+++ app/helpers/transversals_helper.rb
@@ -2,10 +2,10 @@ module TransversalsHelper
   def show_navbar
     if administrator_signed_in?
       render('transversals/navbar_admin')
-    else
-      if renter_signed_in?
-        render('transversals/navbar_renter')
-      end
+    elsif renter_signed_in?
+      render('transversals/navbar_renter')
+    elsif owner_signed_in?
+      render('transversals/navbar_owner')
     end
   end
 end
