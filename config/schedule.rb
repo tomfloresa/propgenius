babd8d8cfc4b8b53c713a767fe3fa7306fbc9de9
Tomás Flores Alvear <tomas.flores.a@gmail.com>
2017-02-07 23:10:52 -0300

    changed time again

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,4 @@
-every 1.day, :at => '20:50' do
+every 1.day, :at => '21:15' do
   runner "Subunit.notify_when_thirty_to_end_lease"
   runner "Subunit.notify_when_fifteen_to_end_lease"
   runner "Subunit.notify_when_five_to_end_lease"
