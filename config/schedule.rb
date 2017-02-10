bc69e43d6ff82fe3a830346d49d1a9675819f258
Tomás Andrés Flores Alvear <tomfloresa@Tomass-MBP.home>
2017-02-10 00:46:20 -0300

    test whenever

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,4 @@
-every 1.day, :at => '23:05' do
+every 2.minutes do
   runner "Subunit.notify_when_thirty_to_end_lease", :environment => 'production'
   runner "Subunit.notify_when_fifteen_to_end_lease", :environment => 'production'
   runner "Subunit.notify_when_five_to_end_lease", :environment => 'production'
