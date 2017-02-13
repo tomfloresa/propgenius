3f69f294a4a6dd244b2b872745ec140bd8d1967b
Tomás Andrés Flores Alvear <tomfloresa@Tomass-MBP.home>
2017-02-12 21:59:39 -0300

    changes to logger

--- config/application.rb
+++ config/application.rb
@@ -7,26 +7,29 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module Propgenius
-  class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-    config.autoload_paths += %W(#{config.root}/lib)
+    class Application < Rails::Application
+        # Settings in config/environments/* take precedence over those specified here.
+        # Application configuration should go into files in config/initializers
+        # -- all .rb files in that directory are automatically loaded.
+        config.autoload_paths += %W(#{config.root}/lib)
 
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+        # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
+        # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+        # config.time_zone = 'Central Time (US & Canada)'
 
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+        # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+        # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+        # config.i18n.default_locale = :de
 
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+        # Do not swallow errors in after_commit/after_rollback callbacks.
+        config.active_record.raise_in_transactional_callbacks = true
 
-    # bower
-    config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
+        # bower
+        config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
 
-    config.time_zone = "Santiago"
-  end
+        config.time_zone = 'Santiago'
+
+        # Set to :debug to see everything in the log.
+        config.log_level = :error
+    end
 end
