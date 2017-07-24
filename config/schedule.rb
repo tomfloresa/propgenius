ef8de7165c2125800c39eff2467249c8ba0cd052
Tomás Andrés Flores Alvear <tomas.flores.a@gmail.com>
2017-07-24 13:33:50 -0400

    modified cron job

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,4 @@
-every 2.minutes do
+every 1.day do
   runner "Subunit.notify_when_thirty_to_end_lease", :environment => 'production'
   runner "Subunit.notify_when_fifteen_to_end_lease", :environment => 'production'
   runner "Subunit.notify_when_five_to_end_lease", :environment => 'production'
